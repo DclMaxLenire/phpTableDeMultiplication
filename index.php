cb48c7a2c8c45fbb8f652f48d9950b690a93f19f
DclMaxLenire <Dcl.maximea@18pixel.fr>
2018-11-09 11:52:46 +0100

    Version avec checkBox ça marche et je peux selectionner plusieur checkbos à la fois

--- index.php
+++ index.php
@@ -12,26 +12,28 @@
         <h1>Table de multiplication</h1>
         <h2> Choissisez la table de multiplication et cliquez sur voir </h2>
         <form id="formulaire" method="GET" name="formulaire">
-            <input type="checkbox" name="selection" id="TableDe01" value="TableDe01">Table de 1
-            <input type="checkbox" name="selection" id="TableDe02" value="TableDe02">Table de 2
-            <input type="checkbox" name="selection" id="TableDe0" value="TableDe03">Table de 3
-            <input type="checkbox" name="selection" id="TableDe0" value="TableDe04">Table de 4
-            <input type="checkbox" name="selection" id="TableDe0" value="TableDe05">Table de 5
-            <input type="checkbox" name="selection" id="TableDe0" value="TableDe06" >Table de 6
-            <input type="checkbox" name="selection" id="TableDe0" value="TableDe07" >Table de 7
-            <input type="checkbox" name="selection" id="TableDe0" value="TableDe08">Table de 8
-            <input type="checkbox" name="selection" id="TableDe0" value="TableDe09">Table de 9
-            <input type="checkbox" name="selection" id="TableDe0" value="TableDe10">Table de 10
+            <input type="checkbox" name="selection[]" id="TableDe01" value="TableDe01" >Table de 1
+            <input type="checkbox" name="selection[]" id="TableDe02" value="TableDe02" >Table de 2
+            <input type="checkbox" name="selection[]" id="TableDe03" value="TableDe03" >Table de 3
+            <input type="checkbox" name="selection[]" id="TableDe04" value="TableDe04" >Table de 4
+            <input type="checkbox" name="selection[]" id="TableDe05" value="TableDe05" >Table de 5
+            <input type="checkbox" name="selection[]" id="TableDe06" value="TableDe06" >Table de 6
+            <input type="checkbox" name="selection[]" id="TableDe07" value="TableDe07" >Table de 7
+            <input type="checkbox" name="selection[]" id="TableDe08" value="TableDe08" >Table de 8
+            <input type="checkbox" name="selection[]" id="TableDe09" value="TableDe09" >Table de 9
+            <input type="checkbox" name="selection[]" id="TableDe10" value="TableDe10" >Table de 10
             <input id="bouton" name="bouton" type="submit">
         </form>
-        <div class="affichageDesTables">
-        <!-- Affiche la table selectionn" -->
+            <!-- Affiche la table selectionné -->
+        <div class="affichageTable">
+            <!-- Affiche la table de 1 à 15 -->
+        <div class="de1a15">
         <?php
                 if ( isSet($_GET['bouton']) ) {
+                    foreach($_GET['selection'] as $valeur){
         // Prend la valeur de la table choisis//
-$tableSelection = $_GET['selection'];
         // Vas chercher les deux dernière caractère de ma valeur //
-$chiffre = substr($tableSelection, -2, 5);
+$chiffre = substr($valeur, -2, 5);
         // Transforme cette valeur en chiffre rond, il supprime le 0//
 $chiffreTable = intval($chiffre);
         // Crée le tableau et calcul //
@@ -40,13 +42,31 @@ $chiffreTable = intval($chiffre);
         $resultat = $chiffreTable*$j;
         echo "<p>$chiffreTable  x $j  =  $resultat <br></p>";
         }
+    }
+}
+?>
+        </div>
+        <!-- Affiche la table de 16 à 30 -->
+        <div class="de16a30">
+        <?php
+                if ( isSet($_GET['bouton']) ) {
+                    foreach($_GET['selection'] as $valeur){
+        // Prend la valeur de la table choisis//
+$tableSelection = $_GET['selection'];
+        // Vas chercher les deux dernière caractère de ma valeur //
+$chiffre = substr($valeur, -2, 5);
+        // Transforme cette valeur en chiffre rond, il supprime le 0//
+$chiffreTable = intval($chiffre);
+        // Crée le tableau et calcul //
     for ($j =16 ; $j <= 30; $j++)
         {   
         $resultat = $chiffreTable*$j;
         echo "<p>$chiffreTable x $j = $resultat <br></p>";
         } 
-    }  
+    } 
+}
 ?>
+    </div>
         </div>
     </section>
 </body>
